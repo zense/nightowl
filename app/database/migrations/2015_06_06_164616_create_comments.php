842dd85ad416df3c17f737e7974e09b12c79585c
bobo <vikasyadavgood@gmail.com>
2015-06-06 22:51:41 +0530

    comments table schema added

---
+++ app/database/migrations/2015_06_06_164616_create_comments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComments extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function($table)
+		{
+			$table->increments('comment_id');
+			$table->integer('post_id')->unsigned();
+			$table->integer('comment_user_id')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
